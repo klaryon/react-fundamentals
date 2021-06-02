19cfa1e77764bcd8d0bac4c10b78254e12a69e45
Clarissa Ardon <clarissa.ardon@flykube.com>
2021-06-02 18:25:31 +0200

    add lottery number states

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-// import { useState } from "react";
+import { useState } from "react";
 import './App.css';
 
 function App()
 {
   //we use const - array destructuring - use State gives us back an array - readOnly number - a function that should change.
-  // const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState(0);
+  const [lotteryNumbers, setLotteryNumbers] = useState([]);
+
+  const addLotteryNumber = () =>
+  {
+    setLotteryNumbers([...lotteryNumbers, number]);
+  }
 
   return (
     <div className="app">
-      <h1>Lets learn the fundamentals</h1>
+      <h1>Let's learn the fundamentals</h1>
 
       <p>Counter</p>
-      <button>+</button>
-      <p>0</p>
-      <button>-</button>
+      <button onClick={() => setNumber(number + 1)}>+</button>
+      <p>{number}</p>
+      <button onClick={() => setNumber(number - 1)}>-</button>
+      <br />
+
+      <button onClick={addLotteryNumber}>Generate lottery number</button>
+      {lotteryNumbers.map((lotteryNumber, i) =>
+      (
+        <p>The lottery number #{i + 1} is {lotteryNumber}</p>
+      ))}
     </div>
   );
 }
