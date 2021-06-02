94137a43e2ecf0cbad8e56bb787667471b466ac6
Clarissa Ardon <clarissa.ardon@flykube.com>
2021-06-02 19:06:01 +0200

    value and onChange value for inputs traditional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+import {useState} from "react";
 import './App.css';
 
 function App()
 {
+  const [firstName, setFirstName] = useState("");
+
+  const onSubmit = (e) =>
+  {
+    e.preventDefault();
+  }
+
   return (
     <div className="app">
       <h1>Contact Me</h1>
       <h2>Please fill out the following</h2>
 
-      <input placeholder="First Name" type="text" /> <br />
-      <input placeholder="Last Name" type="text" /> <br />
-      <input placeholder="Email" type="text" /> <br />
-      <input placeholder="Password" type="text" /> <br />
-      <button>Submit</button>
+      <form>
+        <input
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          placeholder="First Name"
+          type="text" />{" "}
+        <br />
+        <input placeholder="Last Name" type="text" /> <br />
+        <input placeholder="Email" type="email" /> <br />
+        <input placeholder="Password" type="password" /> <br />
+        <button onClick={onSubmit} type="submit">Submit</button>
+      </form>
     </div>
 
   );
