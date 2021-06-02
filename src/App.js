2e0ba69c56c14dcb725ad8693fb074768d031f3e
Clarissa Ardon <clarissa.ardon@flykube.com>
2021-06-02 19:25:40 +0200

    errors message required

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App()
 {
-  const { handleSubmit, register } = useForm();
+  const { handleSubmit, register, formState: {errors}, } = useForm();
 
   const onSubmit = (formData) =>
   {
@@ -12,14 +12,20 @@ function App()
 
   }
 
+  console.log(errors);
+
   return (
     <div className="app">
       <h1>Sign up</h1>
       <h2>Please fill out the following:</h2>
 
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("firstName", {required: true})} placeholder="First Name" type="text" /><br />
-        <input {...register("lastName", {required: true})} placeholder="Last Name" type="text" /> <br />
+        <input {...register("firstName", { required: true })} placeholder="First Name" type="text" />{" "}
+        {errors.firstName?.type === 'required' && 'This field is required'}
+        <br />
+        <input {...register("lastName", { required: true })} placeholder="Last Name" type="text" />{" "}
+        {errors.lastName?.type === 'required' && 'This field is required'}
+        <br />
         <input {...register("email")} placeholder="Email" type="email" /> <br />
         <input {...register("password")} placeholder="Password" type="password" /> <br />
         <button type="submit">Submit</button>
